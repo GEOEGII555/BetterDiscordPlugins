2b1885a15b5e056b6d690de5841641f348ef99d1
Георгий Гришко <57674559+GEOEGII555@users.noreply.github.com>
2022-04-25 14:50:15 +0300

    Create YoutubeInDiscord.plugin.js

---
+++ YoutubeInDiscord.plugin.js
@@ -0,0 +1,16 @@
+/**
+ * @name YoutubeInDiscord
+ * @description Watch YouTube videos without leaving Discord!
+ * @author GEOEGII555
+ * @authorId 848232374918578206
+ */
+class YoutubeInDiscord {
+    start() {
+        if (!global.ZeresPluginLibrary) return window.BdApi.alert("Library Missing",`The library plugin needed for ${this.getName()} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
+        ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "LINK_TO_RAW_CODE");
+    }
+
+    stop() {
+
+	}
+}
